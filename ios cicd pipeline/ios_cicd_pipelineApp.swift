c998d56ebdd26cad11320763677664be3d0ec8f9
Chirag Rathod <chiragrathod@promactinfo.com>
2023-05-08 11:22:21 +0530

    Initial commit

---
+++ ios cicd pipeline/ios_cicd_pipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cicd_pipelineApp.swift
+//  ios cicd pipeline
+//
+//  Created by Chirag on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_cicd_pipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
